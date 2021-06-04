3146047c50c8bb82dce0a8a7c63830cbe27c44b9
渡辺孝之 <na8esin.mpst@gmail.com>
2021-06-05 08:11:39 +0900

    OrderViewModel 集荷日がinitじゃないと初期化できないから、初期化処理変更

--- app/src/main/java/com/example/cupcake/model/OrderViewModel.kt
+++ app/src/main/java/com/example/cupcake/model/OrderViewModel.kt
@@ -8,28 +8,40 @@ import java.util.*
 
 class OrderViewModel : ViewModel() {
     // このgetter,setterの組み合わせを表現できるアノテーションがあってもいいと思う
-    private val _quantity = MutableLiveData<Int>(0)
+    private val _quantity = MutableLiveData<Int>()
     val quantity: LiveData<Int> = _quantity
     fun setQuantity(numberCupcakes: Int) {
         _quantity.value = numberCupcakes
     }
 
-    private val _flavor = MutableLiveData<String>("")
+    private val _flavor = MutableLiveData<String>()
     val flavor: LiveData<String> = _flavor
     fun setFlavor(desiredFlavor: String) {
         _flavor.value = desiredFlavor
     }
 
-    private val _date = MutableLiveData<String>("")
+    private val _date = MutableLiveData<String>()
     val date: LiveData<String> = _date
     fun setDate(pickupDate: String) {
         _date.value = pickupDate
     }
 
-    private val _price = MutableLiveData<Double>(0.0)
+    private val _price = MutableLiveData<Double>()
     val price: LiveData<Double> = _price
 
     val dateOptions = getPickupOptions()
+
+    init {
+        resetOrder()
+    }
+
+    fun resetOrder() {
+        _quantity.value = 0
+        _flavor.value = ""
+        // こいつだけ、initのタイミングじゃないと初期化できないのか
+        _date.value = dateOptions[0]
+        _price.value = 0.0
+    }
 
     fun hasNoFlavorSet(): Boolean {
         return _flavor.value.isNullOrEmpty()
