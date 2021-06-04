7e3aa1a681e445b623b5602a9de15493712379b8
渡辺孝之 <na8esin.mpst@gmail.com>
2021-06-05 07:54:52 +0900

    集荷可能日を４つ作ってxmlに埋め込む

--- app/src/main/java/com/example/cupcake/model/OrderViewModel.kt
+++ app/src/main/java/com/example/cupcake/model/OrderViewModel.kt
@@ -3,33 +3,47 @@ package com.example.cupcake.model
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import java.text.SimpleDateFormat
+import java.util.*
 
 class OrderViewModel : ViewModel() {
+    // このgetter,setterの組み合わせを表現できるアノテーションがあってもいいと思う
     private val _quantity = MutableLiveData<Int>(0)
     val quantity: LiveData<Int> = _quantity
-
-    private val _flavor = MutableLiveData<String>("")
-    val flavor: LiveData<String> = _flavor
-
-    private val _date = MutableLiveData<String>("")
-    val date: LiveData<String> = _date
-
-    private val _price = MutableLiveData<Double>(0.0)
-    val price: LiveData<Double> = _price
-
     fun setQuantity(numberCupcakes: Int) {
         _quantity.value = numberCupcakes
     }
 
+    private val _flavor = MutableLiveData<String>("")
+    val flavor: LiveData<String> = _flavor
     fun setFlavor(desiredFlavor: String) {
         _flavor.value = desiredFlavor
     }
 
+    private val _date = MutableLiveData<String>("")
+    val date: LiveData<String> = _date
     fun setDate(pickupDate: String) {
         _date.value = pickupDate
     }
 
+    private val _price = MutableLiveData<Double>(0.0)
+    val price: LiveData<Double> = _price
+
+    val dateOptions = getPickupOptions()
+
     fun hasNoFlavorSet(): Boolean {
         return _flavor.value.isNullOrEmpty()
+    }
+
+    private fun getPickupOptions(): List<String> {
+        val options = mutableListOf<String>()
+        val formatter = SimpleDateFormat("E MMM d", Locale.getDefault())
+        val calendar = Calendar.getInstance()
+        // Create a list of dates starting with the current date and the following 3 dates
+        repeat(4) {
+            options.add(formatter.format(calendar.time))
+            calendar.add(Calendar.DATE, 1)
+        }
+        return options
     }
 }
